525feda676efc4aefbd4b7d5be40b5032f01e0eb
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-09-02 13:28:38 +0530

    ppo added

---
+++ src/agents/ppo.py
@@ -0,0 +1,308 @@
+import torch
+import torch.nn as nn
+from torch.distributions import MultivariateNormal
+from torch.distributions import Categorical
+import os
+import numpy as np
+from src.utils.logger import logger
+from src.utils.converter import ActionConverter, MADiscActionConverter
+
+
+
+class RolloutBuffer:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+        self.state_values = []
+        self.is_terminals = []
+    
+    def clear(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.logprobs[:]
+        del self.rewards[:]
+        del self.state_values[:]
+        del self.is_terminals[:]
+
+    def __len__(self):
+        return len(self.rewards)
+    
+
+
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim, has_continuous_action_space=False, action_std_init=0.06, network="normal"):
+        super(ActorCritic, self).__init__()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.has_continuous_action_space = has_continuous_action_space
+        
+        if has_continuous_action_space:
+            self.action_dim = action_dim
+            self.action_var = torch.full((action_dim,), action_std_init * action_std_init).to(self.device)
+        # actor
+        if has_continuous_action_space :
+            self.actor = nn.Sequential(
+                            nn.Linear(state_dim, 64),
+                            nn.Tanh(),
+                            nn.Linear(64, 64),
+                            nn.Tanh(),
+                            nn.Linear(64, action_dim),
+                            nn.Tanh()
+                        )
+        else:
+            self.actor = nn.Sequential(
+                                nn.Linear(state_dim, 512),
+                                nn.Tanh(),
+                                nn.Linear(512, 256),
+                                nn.Tanh(),
+                                nn.Linear(256, 64),
+                                nn.Tanh(),
+                                nn.Linear(64, 64),
+                                nn.Tanh(),
+                                nn.Linear(64, action_dim),
+                                nn.Softmax(dim=-1)
+                            )
+            logger.info(f"Network initialized for actor")
+
+                
+        self.critic = nn.Sequential(
+                            nn.Linear(state_dim, 512),
+                            nn.Tanh(),
+                            nn.Linear(512, 256),
+                            nn.Tanh(),
+                            nn.Linear(256, 128),
+                            nn.Tanh(),
+                            nn.Linear(128, 64),
+                            nn.Tanh(),
+                            nn.Linear(64, 1)
+                        )
+
+
+
+        
+    def set_action_std(self, new_action_std):
+        if self.has_continuous_action_space:
+            self.action_var = torch.full((self.action_dim,), new_action_std * new_action_std).to(self.device)
+        else:
+            print("--------------------------------------------------------------------------------------------")
+            print("WARNING : Calling ActorCritic::set_action_std() on discrete action space policy")
+            print("--------------------------------------------------------------------------------------------")
+
+    def forward(self):
+        raise NotImplementedError
+    
+    def act(self, state):
+
+        if self.has_continuous_action_space:
+            action_mean = self.actor(state)
+            cov_mat = torch.diag(self.action_var).unsqueeze(dim=0)
+            dist = MultivariateNormal(action_mean, cov_mat)
+        else:
+            action_probs = self.actor(state)
+            dist = Categorical(action_probs)
+
+        action = dist.sample()
+        action_logprob = dist.log_prob(action)
+        state_val = self.critic(state)
+
+        return action.detach(), action_logprob.detach(), state_val.detach()
+    
+    def evaluate(self, state, action):
+
+        if self.has_continuous_action_space:
+            action_mean = self.actor(state)
+            
+            action_var = self.action_var.expand_as(action_mean)
+            cov_mat = torch.diag_embed(action_var).to(self.device)
+            dist = MultivariateNormal(action_mean, cov_mat)
+            
+            # For Single Action Environments.
+            if self.action_dim == 1:
+                action = action.reshape(-1, self.action_dim)
+        else:
+            action_probs = self.actor(state)
+            dist = Categorical(action_probs)
+        action_logprobs = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+        state_values = self.critic(state)
+        
+        return action_logprobs, state_values, dist_entropy
+    
+
+
+
+
+class PPO:
+    def __init__(self, state_dim, sublist, env, config):
+        self.config = config
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.has_continuous_action_space = self.config['has_continuous_action_space']
+
+        if sublist is not None:
+            self.ac = MADiscActionConverter(env, sublist)
+            action_dim = self.ac.action_size()
+            logger.info(f"Using MADiscActionConverter with action size: {action_dim}")
+        else:
+            self.ac = ActionConverter(env)
+            action_dim = self.ac.n
+            logger.info(f"Using ActionConverter with action size: {action_dim}")
+
+        if self.config['has_continuous_action_space']:
+            self.action_std = self.config['action_std_init']
+
+        self.gamma = self.config['gamma']
+        self.eps_clip = self.config['eps_clip']
+        self.K_epochs = self.config['K_epochs']
+        
+        self.buffer = RolloutBuffer()
+
+        self.policy = ActorCritic(state_dim, action_dim, network=self.config['network']).to(self.device)
+        self.optimizer = torch.optim.Adam([
+                        {'params': self.policy.actor.parameters(), 'lr': self.config['lr_actor']},
+                        {'params': self.policy.critic.parameters(), 'lr': self.config['lr_critic']}
+                    ])
+
+        self.policy_old = ActorCritic(state_dim, action_dim, network=self.config['network']).to(self.device)
+        self.policy_old.load_state_dict(self.policy.state_dict())
+        
+        self.MseLoss = nn.MSELoss()
+
+    def set_action_std(self, new_action_std):
+        if self.has_continuous_action_space:
+            self.action_std = new_action_std
+            self.policy.set_action_std(new_action_std)
+            self.policy_old.set_action_std(new_action_std)
+        else:
+            print("--------------------------------------------------------------------------------------------")
+            print("WARNING : Calling PPO::set_action_std() on discrete action space policy")
+            print("--------------------------------------------------------------------------------------------")
+
+    def decay_action_std(self, action_std_decay_rate, min_action_std):
+        print("--------------------------------------------------------------------------------------------")
+        if self.has_continuous_action_space:
+            self.action_std = self.action_std - action_std_decay_rate
+            self.action_std = round(self.action_std, 4)
+            if (self.action_std <= min_action_std):
+                self.action_std = min_action_std
+                print("setting actor output action_std to min_action_std : ", self.action_std)
+            else:
+                print("setting actor output action_std to : ", self.action_std)
+            self.set_action_std(self.action_std)
+
+        else:
+            print("WARNING : Calling PPO::decay_action_std() on discrete action space policy")
+        print("--------------------------------------------------------------------------------------------")
+
+    def get_buffer_size(self):
+        return len(self.buffer)
+
+    def select_action(self, state):
+        if not isinstance(state, np.ndarray):
+            # Optionally raise an error to prevent silent bugs:
+            raise TypeError(f"Expected state to be a numpy.ndarray but got {type(state)} instead.")
+
+        if self.has_continuous_action_space:
+            with torch.no_grad():
+                state = torch.FloatTensor(state).to(self.device)
+                action, action_logprob, state_val = self.policy_old.act(state)
+
+            return action.detach().cpu().numpy().flatten()
+        else:
+            with torch.no_grad():
+                state = torch.FloatTensor(state).to(self.device)
+                action, action_logprob, state_val = self.policy_old.act(state)
+            
+            #self.buffer.states.append(state)
+            #self.buffer.actions.append(action)
+            #self.buffer.logprobs.append(action_logprob)
+            #self.buffer.state_values.append(state_val)
+
+            action = action.item()
+            grid_action = self.ac.act(action)
+            return action, grid_action, action_logprob, state_val, state
+
+    def update(self):
+        # Monte Carlo estimate of returns
+        rewards = []
+        discounted_reward = 0
+        for reward, is_terminal in zip(reversed(self.buffer.rewards), reversed(self.buffer.is_terminals)):
+            if is_terminal:
+                discounted_reward = 0
+            discounted_reward = reward + (self.gamma * discounted_reward)
+            rewards.insert(0, discounted_reward)
+            
+        # Normalizing the rewards
+        rewards = torch.tensor(rewards, dtype=torch.float32).to(self.device)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-7)
+
+        # convert list to tensor
+        old_states = torch.squeeze(torch.stack(self.buffer.states, dim=0)).detach().to(self.device)
+        old_actions = torch.squeeze(torch.stack(self.buffer.actions, dim=0)).detach().to(self.device)
+        old_logprobs = torch.squeeze(torch.stack(self.buffer.logprobs, dim=0)).detach().to(self.device)
+        old_state_values = torch.squeeze(torch.stack(self.buffer.state_values, dim=0)).detach().to(self.device)
+
+        # calculate advantages
+        logger.info(f"rewards shape: {rewards.shape}, old_state_values shape: {old_state_values.shape}")
+        advantages = rewards.detach() - old_state_values.detach()
+
+        # Optimize policy for K epochs
+        for _ in range(self.K_epochs):
+
+            # Evaluating old actions and values
+            logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
+
+            # match state_values tensor dimensions with rewards tensor
+            state_values = torch.squeeze(state_values)
+            
+            # Finding the ratio (pi_theta / pi_theta__old)
+            ratios = torch.exp(logprobs - old_logprobs.detach())
+
+            # Finding Surrogate Loss  
+            surr1 = ratios * advantages
+            surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
+
+            # final loss of clipped objective PPO
+            loss = -torch.min(surr1, surr2) + 0.5 * self.MseLoss(state_values, rewards) - 0.01 * dist_entropy
+            
+            # take gradient step
+            self.optimizer.zero_grad()
+            loss.mean().backward()
+            torch.nn.utils.clip_grad_norm_(self.policy.parameters(), max_norm=0.5)
+            self.optimizer.step()
+            
+        # Copy new weights into old policy
+        self.policy_old.load_state_dict(self.policy.state_dict())
+
+        # clear buffer
+        self.buffer.clear()
+    
+    def save(self, checkpoint_path, filename="ppo_checkpoint.pth"):
+        if checkpoint_path:
+            os.makedirs(checkpoint_path, exist_ok=True)
+        checkpoint = {
+            'model_state_dict': self.policy_old.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'config': self.config
+        }
+        save_path = os.path.join(checkpoint_path, filename)
+        torch.save(checkpoint, save_path)
+        logger.info(f"[SAVE] Checkpoint saved to {save_path}")
+
+   
+    def load(self, checkpoint_path, filename="ppo_checkpoint.pth"):
+        file = os.path.join(checkpoint_path, filename)
+        checkpoint = torch.load(file, map_location=self.device)
+
+        self.policy_old.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.policy.load_state_dict(checkpoint['model_state_dict'])
+        logger.info(f"[LOAD] Checkpoint loaded from {file}")
+        return True
+
+
+
+
